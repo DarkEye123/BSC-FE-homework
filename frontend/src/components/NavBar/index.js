a88b96132ed80111ad4cb0202026174fa9a1ed9e
DarkEye <lesko.matej.pu@gmail.com>
2019-03-27 10:08:44 +0100

    fixes + removed comments

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -2,47 +2,8 @@ import React, { useEffect, useRef } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Header, Logo } from './styles';
 
-// const TRANSITION = 'top 0.5s';
-// const BUFFER_SIZE = 22; // seems to be 2 scrolls = 22 calls
-// const myRef = useRef();
-// let lastScrollY = 0;
-// let isRendered = true;
-// let cntUp = 0;
-// const clearHeader = () => {
-//   cntUp = 0;
-//   myRef.current.style.top = `-${myRef.current.offsetHeight}px`;
-//   isRendered = false;
-// };
-// const renderHeader = () => {
-//   if (!isRendered) {
-//     isRendered = true;
-//     myRef.current.style.top = '0px';
-//   }
-// };
-// const handleScroll = () => {
-//   const diff = lastScrollY - window.scrollY;
-//   const goingUp = diff > 0;
-//   const goingDown = diff < 0;
-//   const isOnLimit = window.scrollY <= myRef.current.offsetHeight;
-//   if (goingUp) {
-//     cntUp++;
-//   } else if (goingDown) {
-//     clearHeader();
-//   }
-//   if (cntUp >= BUFFER_SIZE || isOnLimit) {
-//     renderHeader();
-//   }
-//   lastScrollY = window.scrollY;
-// };
-// useEffect(() => {
-//   window.addEventListener('scroll', handleScroll);
-//   return () => {
-//     window.removeEventListener('scroll', handleScroll);
-//   };
-// }, []);
 const NavBar = () => (
   <Router>
-    {/* <Header ref={myRef} transition={TRANSITION}> */}
     <Header>
       <Logo>
         <Link to="/" />
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,3 +2,5 @@ export { CenteredDiv, CenteredMain } from './common/Centered';
 export { default as NavBar } from './NavBar';
 export { default as Login } from './Login';
 export { default as InputField } from './common/InputField';
+export { default as Button } from './common/Button/Normal';
+export { default as SpinningButton } from './common/Button/Spinning';
